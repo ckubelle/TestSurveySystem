44ee32c569d9dc22da47aefce31b1417f4c11451
William Escamilla <wescamilla10@gmail.com>
2021-04-14 19:55:29 -0400

    Displaying forms the user has created, able to pull more information from firestore when clicking edit, however current question component architectureis ruthless

--- angular/src/app/edit/edit.component.ts
+++ angular/src/app/edit/edit.component.ts
@@ -1,34 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  DocumentSnapshot,
+  QueryDocumentSnapshot,
+} from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatFormField, MAT_FORM_FIELD } from '@angular/material/form-field';
 import { MatListOption } from '@angular/material/list';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { QuestionComponent } from '../question/question.component';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
   id: string;
-  allForms: Array<any>;
+  allFormsArray: Array<any>;
 
-  constructor(
-  private afs: AngularFirestore, 
-  private route: ActivatedRoute
-  ) 
-  {this.route.params.subscribe(params=>{
-    this.id = params.id;
-  })}
+  testTitle: string;
+  formTypeSelection: string;
+  testCreator: string;
+  questions: FormArray;
+  selectedId: string;
+  questionLength: number;
 
-  ngOnInit(): void {
-    this.afs.collection("tests").valueChanges()
-      .subscribe(formsList => {
-        this.allForms = formsList as Array<any>
+  currentDocument: QueryDocumentSnapshot<any>;
+
+  constructor(private afs: AngularFirestore, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
     });
-    console.log(this.id);
   }
 
+  ngOnInit(): void {
+    this.afs
+      .collection('tests')
+      .valueChanges()
+      .subscribe((formsList) => {
+        this.allFormsArray = formsList as Array<any>;
+        console.log(typeof this.allFormsArray);
+      });
+  }
+
+  async getQuestions(indiForm) {
+    await this.afs
+      .collection('tests')
+      .snapshotChanges()
+      .subscribe(async (documentList) => {
+        try {
+          await documentList.some(async (document) => {
+            this.currentDocument = await document.payload.doc;
+            this.testTitle = await this.currentDocument.get('testTitle');
+            this.formTypeSelection = await this.currentDocument.get(
+              'formTypeSelection'
+            );
+            this.testCreator = await this.currentDocument.get('testCreator');
+            await this.questions.patchValue(
+              this.currentDocument.get('questions')
+            );
+            this.questionLength = await this.questions.length;
+            if (
+              this.formTypeSelection == indiForm.formTypeSelection &&
+              this.questions.length == indiForm.questions.length &&
+              this.testCreator == indiForm.testCreator &&
+              this.testTitle == indiForm.testTitle
+            ) {
+              this.selectedId = this.currentDocument.id;
+              throw new Error();
+            }
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  }
 }
--- angular/src/app/question/question.component.ts
+++ angular/src/app/question/question.component.ts
@@ -1,123 +1,178 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { FormBuilder, FormGroup, FormArray, Validators} from '@angular/forms';
+import {
+  AngularFirestore,
+  QueryDocumentSnapshot,
+} from '@angular/fire/firestore';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { MatListOption } from '@angular/material/list';
 import { ActivatedRoute, Router } from '@angular/router';
 import { tap, first } from 'rxjs/operators';
-import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatFormField, MAT_FORM_FIELD } from '@angular/material/form-field';
-
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
   id: string;
-  myForm : FormGroup;
-
+  myForm: FormGroup;
+  question: FormControl;
 
   loading = false;
   success = false;
 
   constructor(
-    private afs: AngularFirestore, 
+    private afs: AngularFirestore,
     private route: ActivatedRoute,
     private fb: FormBuilder
-    ) {this.route.params.subscribe(params=>{
+  ) {
+    this.route.params.subscribe((params) => {
       this.id = params.id;
-    })}
-
-
-  ngOnInit(): void {
-    this.myForm = this.fb.group({
-      testTitle: ['', [
-        Validators.required,
-      ]],
-      formTypeSelection: ['', [
-        Validators.required,
-      ]],
-      testCreator: this.id,
-      questions: this.fb.array([])
     });
   }
 
-//Getter for all of the questions in a form
-  get questionForms(){
-    return this.myForm.get('questions') as FormArray
+  ngOnInit(): void {
+    this.question = this.fb.control({
+      questionTitle: [''],
+      questionAnswer: [''],
+      questionType: [''],
+      mcoption1: [],
+      mcoption2: [],
+      mcoption3: [],
+      mcoption4: [],
+      rank1: [],
+      rank2: [],
+      rank3: [],
+      rank4: [],
+      rank5: [],
+      rankAnswer1: [],
+      rankAnswer2: [],
+      rankAnswer3: [],
+      rankAnswer4: [],
+      rankAnswer5: [],
+      leftanswer1: [],
+      rightanswer1: [],
+      leftanswer2: [],
+      rightanswer2: [],
+    });
+    this.myForm = this.fb.group({
+      //testTitle: ['', [Validators.required]],
+      //formTypeSelection: ['', [Validators.required]],
+      testTitle: new FormControl(['']),
+      formTypeSelection: new FormControl(['']),
+      testCreator: new FormControl(this.id),
+      questions: this.fb.array([this.question]),
+    });
   }
 
-//Getter for the title of the test  
-  get testTitle(){
-      return this.myForm.get('testTitle');
+  //Getter for all of the questions in a form
+  get questionForms() {
+    return this.myForm.get('questions') as FormArray;
   }
 
-//Getter for the type of form (quiz or test)
-  get formTypeSelection(){
+  set questionForms(questions: FormArray) {
+    this.myForm['questions'] = questions;
+  }
+
+  //Getter for the title of the test
+  get testTitle() {
+    return this.myForm.get('testTitle');
+  }
+
+  //Getter for the type of form (quiz or test)
+  get formTypeSelection() {
     return this.myForm.get('formTypeSelection');
-}
+  }
 
-//Getter for the answer of each question
-get questionAnswer(){
-  return this.myForm.get('questionAnswer');
-}
+  //Getter for the answer of each question
+  get questionAnswer() {
+    return this.myForm.get('questionAnswer');
+  }
 
-//function to add a question to the form (connects to the button)
-  addQuestion(){
+  //function to add a question to the form (connects to the button)
+  addQuestion() {
+    console.log(this.myForm);
+
+    const question = this.fb.group({
+      questionTitle: [],
+      questionAnswer: [],
+      questionType: [],
+      mcoption1: [],
+      mcoption2: [],
+      mcoption3: [],
+      mcoption4: [],
+      rank1: [],
+      rank2: [],
+      rank3: [],
+      rank4: [],
+      rank5: [],
+      rankAnswer1: [],
+      rankAnswer2: [],
+      rankAnswer3: [],
+      rankAnswer4: [],
+      rankAnswer5: [],
+      leftanswer1: [],
+      rightanswer1: [],
+      leftanswer2: [],
+      rightanswer2: [],
+    });
+    this.questionForms.push(question);
+  }
+
+  //function to delete a question (connects to button)
+  deleteQuestion(question) {
+    this.questionForms.removeAt(question);
+  }
+
+  async preloadData(
+    title: string,
+    selection: string,
+    qs: FormArray,
+    questionLength: number,
+    selectedId: string,
+    currentDocument: QueryDocumentSnapshot<any>
+  ) {
+    this.id = await currentDocument.get('testCreator');
+    await this.testTitle.patchValue(currentDocument.get('testTitle'));
+    await this.formTypeSelection.patchValue(
+      currentDocument.get('formTypeSelection')
+    );
+    this.questionForms = currentDocument.get('questions');
+    while (this.questionForms.length) {
+      this.questionForms.removeAt(0);
+    }
+
+    currentDocument.get('questions').array.forEach((question) => {
+      this.questionForms.push(question);
+    });
+    //for (let i = 0; i < questionLength; i++) {
+    //this.questionForms.push(qs.value.at(i));
+    //}
 
     console.log(this.myForm);
-    
-    const question = this.fb.group({
-        questionTitle: [], 
-        questionAnswer: [],
-        questionType: [],
-        mcoption1: [],
-        mcoption2: [],
-        mcoption3: [],
-        mcoption4: [],
-        rank1: [],
-        rank2: [],
-        rank3: [],
-        rank4: [],
-        rank5: [],
-        rankAnswer1: [],
-        rankAnswer2: [],
-        rankAnswer3: [],
-        rankAnswer4: [],
-        rankAnswer5: [],
-        leftanswer1: [],
-        rightanswer1: [],
-        leftanswer2: [],
-        rightanswer2: [],
-    })
-    this.questionForms.push(question);
-
-  }
-  
-//function to delete a question (connects to button)
-  deleteQuestion(question){
-    this.questionForms.removeAt(question);
-   
   }
 
-//function to post test/survey information to Firebase
-  async submitHandler(){
+  //function to post test/survey information to Firebase
+  async submitHandler() {
     this.loading = true;
 
     const formValue = this.myForm.value;
 
-    try{
+    try {
       await this.afs.collection('tests').add(formValue);
       this.success = true;
-    }
-    catch(err){
+    } catch (err) {
       console.log(err);
     }
 
     this.loading = false;
   }
-
 }
-
-
